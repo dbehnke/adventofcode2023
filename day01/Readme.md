3d925ee711034c93dd2b4cf20f49459ce95efca8
Dave Behnke <916775+dbehnke@users.noreply.github.com>
2023-12-02 00:30:27 -0500

    add part 2

--- day01/Readme.md
+++ day01/Readme.md
@@ -3,7 +3,8 @@
 ```
 ❯ cargo run 
    Compiling day01 v0.1.0 (/Users/dbehnke/development/adventofcode2023/day01)
-    Finished dev [unoptimized + debuginfo] target(s) in 0.10s
+    Finished dev [unoptimized + debuginfo] target(s) in 0.11s
      Running `target/debug/day01`
-Sum of all two digit numbers in input.txt is 54561
+The answer for part one is: 54561
+The answer for part two is: 54076
 ```
--- day01/src/main.rs
+++ day01/src/main.rs
@@ -30,7 +30,46 @@ fn get_two_digit_from_code(s: &str) -> i32 {
     first_digit * 10 + last_digit
 }
 
-fn main() {
+fn get_two_digit_from_code_with_words(s: &str) -> i32 {
+    //replace all words with numbers
+
+    /*
+    o n   e eight
+    t w   o one
+    t hre e eight
+    f ou  r 
+    f iv  e eight
+    s i   x 
+    s eve n nine
+    e igh t two
+    n inn e eight
+
+    
+     */
+
+    let mut s = s.replace("oneight", "oneeight");
+    s = s.replace("twone", "twoone");
+    s = s.replace("threeight", "threeeight");
+    s = s.replace("fiveight", "fiveeight");
+    s = s.replace("sevenine", "sevennine");
+    s = s.replace("eightwo", "eighttwo");
+    s = s.replace("eighthree", "eightthree");
+    s = s.replace("nineight", "nineeight");
+    s = s.replace("one", "1");
+    s = s.replace("two", "2");
+    s = s.replace("three", "3");
+    s = s.replace("four", "4");
+    s = s.replace("five", "5");
+    s = s.replace("six", "6");
+    s = s.replace("seven", "7");
+    s = s.replace("eight", "8");
+    s = s.replace("nine", "9");
+    //println!("s: {} answer: {}", s, get_two_digit_from_code(s.as_str()));
+
+    return get_two_digit_from_code(s.as_str());
+}
+
+fn part_one() -> i32 {
     // File input.txt must exist in the current path
     if let Ok(lines) = read_lines("./input.txt") {
         let mut sum = 0;
@@ -41,6 +80,27 @@ fn main() {
                 sum += digits;
             }
         }
-        println!("Sum of all two digit numbers in input.txt is {}", sum);
+        return sum;
     }
+    0
+}
+
+fn part_two() -> i32 {
+    if let Ok(lines) = read_lines("./input.txt") {
+        let mut sum = 0;
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(l) = line {
+                let digits = get_two_digit_from_code_with_words(&l);
+                sum += digits;
+            }
+        }
+        return sum;
+    }
+    0
+}
+
+fn main() {
+    println!("The answer for part one is: {}", part_one());
+    println!("The answer for part two is: {}", part_two());
 }
